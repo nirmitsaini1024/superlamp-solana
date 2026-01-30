b3c96e7e64a2b1094e7dd72ab0fa8232098cfda2
Nirmit Saini <nirmitsaini24@gmail.com>
2026-01-30 12:48:47 +0530

    feat: setup Prisma schema and database models

---
+++ db/index.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "../lib/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+
+const prisma = global.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export default prisma;
